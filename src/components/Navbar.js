cb3acd06021da18f3175d851e0fd306388a15d10
mariamnozadze <mariamnozadze97@yahoo.com>
2022-12-12 21:40:45 +0400

    - Fixed functions - Added hooks & clickhandlers

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export class Navbar extends Component {
   render = () => (
     <div className="col-12">
       <h2 className="bg-primary text-white text-center p-2">
-        {this.state.userName}'s To Do List
+        {this.props.name}'s To Do List
       </h2>
     </div>
   );
---
+++ src/components/TodoRows.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class todoRows extends Component {
+  render = () => (
+    <tr key={this.props.item.action}>
+      <td>{this.props.item.action}</td>
+      <td>
+        <input
+          type="checkbox"
+          checked={this.props.item.done}
+          onChange={() => this.props.callback(this.props.item)}
+        ></input>
+      </td>
+    </tr>
+  );
+}
